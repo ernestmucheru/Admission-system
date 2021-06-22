990225020beca2b7181372f1b939b1f84a967047
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-22 16:34:40 +0300

    Added the profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to Admission system","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Instant Admission system","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form,title=title)
